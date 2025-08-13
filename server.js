e68619d22101284b36164afe91f4d8a9d0400db2
dominic6644 <mr.wolfgang17@yahoo.com>
2025-08-13 13:50:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -260,3 +260,9 @@ app.get('/api/protected', isAuthenticated, (req, res) => {
     res.json({ message: 'This is a protected route' });
 });
 
+app.get('/ads.txt', (req, res) => {
+  res.redirect(301, 'https://srv.adstxtmanager.com/19390/dirtbikefinderuk.co.uk');
+});
+
+
+
