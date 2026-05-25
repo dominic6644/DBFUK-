49b14c11d8f9cb3cda2e4e62c63d802556d0e01a
dominic6644 <mr.wolfgang17@yahoo.com>
2026-05-25 21:23:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -117,9 +117,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
