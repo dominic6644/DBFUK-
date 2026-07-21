721b5de1f3ee1c5c7406045f9e9fe9dc4c5f7977
dominic6644 <mr.wolfgang17@yahoo.com>
2026-07-21 20:19:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -815,6 +815,9 @@ res.send(`
 <meta property="og:description" content="${description}">
 <meta property="og:url" content="https://dirtbikefinderuk.co.uk/news/${post.subcategory}/${post.slug}">
 <meta property="og:image" content="${imageUrl}">
+<meta property="og:image:secure_url" content="${imageUrl}">
+<meta property="og:image:type" content="image/webp">
+<meta property="og:image:alt" content="${post.title}">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="630">
 <meta property="og:site_name" content="Dirt Bike Finder UK">
